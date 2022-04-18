ce273b23727a5cca23881274ed74d04238eae38e
ourainbows <casasjuanfelipe@gmail.com>
2022-04-18 01:08:59 -0500

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
- echo 'https://ourainbows.codes' > CNAME
+echo 'https://ourainbows.codes' > CNAME
 
 git init
 git checkout -b main
